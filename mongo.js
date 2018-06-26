b21d19f53de9a17a5cdc9f54a57959ae32e63883
Tomas Vermilä <tomas.vermila@myy.haaga-helia.fi>
2018-06-26 23:36:03 +0300

    tehtävä 3.12 tehty

--- mongo.js
+++ mongo.js
@@ -12,14 +12,30 @@ const Person = mongoose.model('Person', {
   number: String
 })
 
-const person = new Person({
-  name: 'Testi Apina',
-  number: "050 - 444 6789"
-})
-
-person
+//uuden henkilön tallennus kantaan
+if(process.argv.length > 2){
+    const nimi = process.argv[2]
+    const numero = process.argv[3]
+    const person = new Person({
+        name: nimi,
+        number: numero
+    })
+    console.log(`lisätään henkilö ${person.name} numero ${person.number} luetteloon`)
+  person
   .save()
   .then(response => {
     console.log('person saved!')
     mongoose.connection.close()
   })
+}else {
+  //tietojen lukeminen kannasta, jos ei annettu uuden henkilön tietoja
+  Person
+  .find({})
+  .then(result => {
+    console.log('puhelinluettelo:')
+      result.forEach(person => {          
+          console.log(`${person.name} ${person.number}`)
+      });
+      mongoose.connection.close()
+  })
+}
