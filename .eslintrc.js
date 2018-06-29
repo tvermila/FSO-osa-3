cffe903580b0d76811eeb2e267082b08cfdd21be
Tomas Vermilä <tomas.vermila@myy.haaga-helia.fi>
2018-06-29 21:53:09 +0300

    mongodb:n URI:n käsittelyä muutettu

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,9 @@
 module.exports = {
     "env": {
-        "browser": true,
         "es6": true,
         "node": true
     },
     "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2015,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
     "rules": {
         "indent": [
             "error",
